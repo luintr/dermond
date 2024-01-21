774bd1b6227e825905824ee7a6b5aaa66947af5e
croissain <nhattho03@gmail.com>
2024-01-21 18:16:07 +0700

    test

--- src/modules/Story/StoryWork/index.tsx
+++ src/modules/Story/StoryWork/index.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useRef } from 'react';
 import s from './style.module.scss';
 import Container from '@/components/Container';
 import { cinzelFont } from '@/utils/fonts';
 import { storyWorkData } from '@/constants/story/storyWorks';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 const StoryWork = () => {
+  const tabRefs = useRef<Array<HTMLDivElement | null>>([]);
+
+  useEffect(() => {
+    if (!tabRefs.current) return;
+    const ctx = gsap.context(() => {
+      ScrollTrigger.create({
+        trigger: tabRefs.current[0],
+        start: 'top top+=100',
+        end: 'bottom bottom',
+        pin: true,
+        markers: true,
+        scrub: false,
+        onEnter: () => {
+          console.log('enter');
+        },
+      });
+    }, [tabRefs.current[0]]);
+
+    return () => {
+      ctx.kill();
+    };
+  }, [tabRefs]);
+
   return (
     <section className={s.storyWork}>
       <Container className={s.container}>
@@ -14,7 +42,11 @@ const StoryWork = () => {
 
         <div className={s.workList}>
           {storyWorkData.map((item, index) => (
-            <div key={item.id} className={`${s.workItem} grid grid-cols-12`}>
+            <div
+              key={item.id}
+              className={`${s.workItem} grid grid-cols-12`}
+              ref={el => (tabRefs.current[index] = el)}
+            >
               <div className={`${s.workItem_top} col-span-12 col-start-1`}>
                 <span className={s.workItem_number}>{`0${item.id}.`}</span>
                 <p className={`${s.workItem_title} ${cinzelFont.className}`}>
