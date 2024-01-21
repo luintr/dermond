b26646ee46e9d4689da0d3b395e6d307b24ca080
luintr <hainam.huflit@gmail.com>
2024-01-21 16:12:32 +0700

    ci: change api url

--- next.config.js
+++ next.config.js
@@ -2,19 +2,6 @@
 
 const path = require('path');
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    block-all-mixed-content;
-    upgrade-insecure-requests;
-`;
 
 const nextConfig = {
   swcMinify: true,
@@ -25,19 +12,6 @@ const nextConfig = {
     @import "@Styles/_tools.scss";
     @import "@Styles/_setting.scss";
    `,
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, ''),
-          },
-        ],
-      },
-    ];
   },
 };
 
