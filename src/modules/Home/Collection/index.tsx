9a6f6236473eced603747b72179daccbdd13ef36
iceTgenerative <135957914+iceTgenerative@users.noreply.github.com>
2024-02-06 12:44:45 +0700

    update: vertical marquee + animate home collection

--- src/modules/Home/Collection/index.tsx
+++ src/modules/Home/Collection/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client';
+
+import React, { useRef } from 'react';
 import s from './style.module.scss';
 import Container from '@Components/Container';
 import { cinzelFont, playfairFont } from '@/utils/fonts';
@@ -7,23 +9,41 @@ import { HOME_COLLECTION_DATA } from '@/constants/homeData/data';
 import MarqueeText from '@/modules/Home/Collection/MarqueeText';
 import BoxParallax from '@/components/BoxParallax';
 import Fade from '@/components/Fade';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const CollectionSecion = () => {
+  const line = useRef(null);
+
+  useGSAP(() => {
+    gsap.to(line.current, {
+      clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+      ease: 'power4.out',
+      duration: 1.3,
+      scrollTrigger: {
+        trigger: line.current,
+        start: 'top 80%',
+        end: 'bottom center',
+      },
+    });
+  });
   return (
     <section className={s.collectionSection}>
       <Container className={s.container}>
         <div className={`${s.introduce} grid grid-cols-12`}>
-          <h2
-            className={`${s.introduce_title} ${cinzelFont.className} col-span-6 col-start-2`}
-          >
-            MINIMALIST STRONG FOR <span>COLLECTION</span>
-          </h2>
+          <Fade direction={'bottom'} from={'30px'} delayTrigger={0.2}>
+            <h2
+              className={`${s.introduce_title} ${cinzelFont.className} col-span-6 col-start-2`}
+            >
+              MINIMALIST STRONG FOR <span>COLLECTION</span>
+            </h2>
+          </Fade>
           <div
             className={`${s.introduce_desc} col-span-4 col-start-8 grid grid-cols-4`}
           >
-            <div className={`${s.line} col-span-4`}></div>
+            <div className={`${s.line} col-span-4`} ref={line}></div>
             <div className={`${s.wrapText} col-span-3 col-start-2`}>
-              <Fade direction={'bottom'} from={'30px'} delayTrigger={0.2}>
+              <Fade direction={'bottom'} from={'30px'} delayTrigger={0.3}>
                 <p>
                   Emphasize{' '}
                   <span className={playfairFont.className}>DER MOND</span>
