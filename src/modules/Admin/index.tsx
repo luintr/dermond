051e9a2aa163383056463b6b318f961b989d50f7
Luin <luintr.work@gmail.com>
2024-02-18 17:45:56 +0700

    fix: layout

--- src/modules/Admin/index.tsx
+++ src/modules/Admin/index.tsx
@@ -4,18 +4,18 @@ import React, { useState } from 'react';
 import s from './style.module.scss';
 import AdminTabs from './Tabs';
 import { tabData } from './data';
-import { Card, Divider, Flex } from 'antd';
-import Search from 'antd/es/input/Search';
+import { cinzelFont } from '@/utils/fonts';
 
 const AdminModule = () => {
   const [tab, setTab] = useState<number>(0);
 
   return (
-    <div className={`${s.admin} container grid-cols-12`}>
-      <div className={s.admin_wrapper}>
-        <Flex justify="space-between">
-          <AdminTabs currentTab={tabData[tab].name} setTab={setTab} />
-        </Flex>
+    <div className={`${s.admin} container`}>
+      <h2 className={`${s.admin_title} ${cinzelFont.className}`}>
+        Admin DashBoard
+      </h2>
+      <div className={`${s.admin_wrapper} `}>
+        <AdminTabs currentTab={tabData[tab].name} setTab={setTab} />
         <div className={s.content}>{tabData[tab].content}</div>
       </div>
     </div>
